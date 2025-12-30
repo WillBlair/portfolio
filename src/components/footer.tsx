98a01a180ca707c2d58eb120e49a69032db53a98
WillBlair <willblair47@gmail.com>
2025-12-29 23:38:52 -0800

    Revert footer font size to text-sm

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,7 +6,7 @@ export function Footer() {
       <div className="container mx-auto pt-6 pb-24">
         <div className="flex flex-col items-center gap-4">
           <Separator className="w-full max-w-md" />
-          <div className="text-xs text-center text-muted-foreground">
+          <div className="text-sm text-center text-muted-foreground">
             Last updated December 2025.
           </div>
         </div>
