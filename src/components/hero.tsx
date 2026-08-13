350d3f8646ad55bead19c957bfece97ddd3a9a8e
Will Blair <willblair47@gmail.com>
2026-08-13 09:27:17 -0700

    fix: update grad to Dec 2026, age to 22

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -48,7 +48,7 @@ export function Hero() {
               </p>
             </div>
             <p className="text-muted-foreground italic text-sm">
-              Expected Graduation May 2027
+              Expected Graduation Dec 2026
             </p>
           </div>
         </div>
